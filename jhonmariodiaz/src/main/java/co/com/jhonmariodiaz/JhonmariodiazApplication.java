7234feffb3bd62938073daa1d8e0f8f116632787
Jhon_Mario <wolfbox22@gmail.com>
2024-08-13 20:36:23 -0500

    first commit

---
+++ jhonmariodiaz/src/main/java/co/com/jhonmariodiaz/JhonmariodiazApplication.java
@@ -0,0 +1,13 @@
+package co.com.jhonmariodiaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhonmariodiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhonmariodiazApplication.class, args);
+	}
+
+}
---
+++ jhonmariodiaz/src/test/java/co/com/jhonmariodiaz/JhonmariodiazApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jhonmariodiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhonmariodiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
